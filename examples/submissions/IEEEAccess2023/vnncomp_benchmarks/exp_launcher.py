acb019bd6cc65cfc32d794156d27f6c467e26fe8
Dario Guidotti <dguidotti@uniss.it>
2023-04-04 08:20:42 +0200

    added exp_launcher.py

--- examples/submissions/IEEEAccess2023/vnncomp_benchmarks/exp_launcher.py
+++ examples/submissions/IEEEAccess2023/vnncomp_benchmarks/exp_launcher.py
@@ -1,3 +1,5 @@
+import time
+
 import pynever.networks as pyn_net
 import pynever.strategies.conversion as pyn_conv
 import pynever.strategies.verification as pyn_ver
@@ -27,7 +29,8 @@ if not os.path.exists(logs_dir):
 logger_exp_stream = logging.getLogger("pynever.strategies.verification")
 logger_exp_file = logging.getLogger("verification_results")
 
-exp_file_handler = logging.FileHandler(f"{logs_dir}VerResults_{datetime.now().strftime('%m.%d.%Y-%H.%M.%S')}.txt")
+log_path = f"{logs_dir}VerResults_{datetime.now().strftime('%m.%d.%Y-%H.%M.%S')}.txt"
+exp_file_handler = logging.FileHandler(log_path)
 exp_stream_handler = logging.StreamHandler()
 
 exp_file_handler.setLevel(logging.INFO)
@@ -44,5 +47,30 @@ logger_exp_stream.setLevel(logging.INFO)
 #
 #
 # ##### PARAMETERS DEFINITION #####
-onnx_path = "onnx_nets/"
-onnx_ids = ["cartpole.onnx", "dubinsrejoin.onnx", "lunarlander.onnx"]
+onnx_path = "clean_onnx/"
+onnx_ids = ["cartpole.onnx"]  # , "dubinsrejoin.onnx", "lunarlander.onnx"]
+verification_modes = ["complete"]
+specs_path = "vnnlib_specs/"
+specs_ids = os.listdir(specs_path)
+timeout = 60
+
+logger_exp_file.info("NET_ID,SPEC_ID,VER_MODE,RESULT,TIME")
+
+for onnx_id in onnx_ids:
+    cs_id = onnx_id.replace(".onnx", "")
+    corresponding_ids = [temp_id for temp_id in specs_ids if cs_id in temp_id]
+    for spec_id in corresponding_ids:
+
+        clean_spec_id = spec_id.replace(".vnnlib", "")
+        for verification_mode in verification_modes:
+            logger_exp_stream.info(f"CHECKING Property: {clean_spec_id} ON Network: {cs_id} "
+                                   f"WITH Mode: {verification_mode}")
+            command = f"timeout {timeout}s python verify_model.py -n {onnx_path + onnx_id} -s {specs_path + spec_id} " \
+                      f"-m {verification_mode} -l {log_path}"
+
+            start = time.perf_counter()
+            res = os.system(command)
+            end = time.perf_counter()
+            if res != 0:
+                logger_exp_file.info(f"{cs_id},{clean_spec_id},{verification_mode},-,-")
+
