d16423455984f7f20c6b1d786a4b5bc132f90fb8
Stefano Demarchi <stefano_demarchi@outlook.com>
2023-05-08 16:24:58 +0200

    Change version name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyNeVer",
-    version="0.0.2.alpha7.patch",
+    version="0.0.2.a8",
     author="Dario Guidotti",
     author_email="dario.guidotti@edu.unige.it",
     license='GNU General Public License with Commons Clause License Condition v1.0',
