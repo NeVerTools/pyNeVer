144a992fb1d78305c0d3431e60e4fe6bc4e7fd50
Stefano Demarchi <stefano_demarchi@outlook.com>
2023-09-07 16:10:35 +0200

    Complete experiments script

--- pynever/tests/soft_computing/exp_launcher.py
+++ pynever/tests/soft_computing/exp_launcher.py
@@ -1,12 +1,14 @@
 # Script for launching the experiments of 'Soft Computing' journal paper
 import csv
 import logging
+import os
+import signal
 import time
+from contextlib import contextmanager
 
 import pynever.strategies.conversion as conv
 from pynever.strategies.conversion import ONNXConverter, ONNXNetwork
 from pynever.strategies.verification import NeVerProperty, NeverVerification
-from pynever.tests.soft_computing.timeout import time_limit, TimeoutException
 
 pynever_setting = [['Over-approx.', 'overapprox', [0]],
                    ['Mixed1', 'mixed', [1]],
@@ -23,6 +25,56 @@ logger_file.setLevel(logging.INFO)
 
 TIMEOUT = 300  # 5 minutes
 
+
+class TimeoutException(Exception):
+    """
+    Exception class for timeout
+
+    """
+
+    pass
+
+
+@contextmanager
+def time_limit(seconds: int):
+    def signal_handler(signum, frame):
+        raise TimeoutException('Timeout')
+
+    signal.signal(signal.SIGALRM, signal_handler)
+    signal.alarm(seconds)
+
+    try:
+        yield
+    finally:
+        signal.alarm(0)
+
+
+def exec_instance(network_path: str, property_path: str, property_id: str):
+    network_instance = conv.load_network_path(network_path)
+    onnx_net = None
+    if isinstance(network_instance, ONNXNetwork):
+        onnx_net = ONNXConverter().to_neural_network(network_instance)
+
+    property_instance = NeVerProperty()
+    property_instance.from_smt_file(property_path)
+
+    for setting in pynever_setting:
+        inst_name = f"[{network_instance.identifier} - {property_id}]"
+        logger_stream.info(f"Benchmark: {inst_name}")
+        logger_stream.info(f"PyNeVer setting: {setting[0]}")
+
+        try:
+            with time_limit(TIMEOUT):
+                strategy = NeverVerification(setting[1], setting[2])
+                time_start = time.perf_counter()
+                safe = strategy.verify(onnx_net, property_instance)
+                time_end = time.perf_counter()
+                logger_file.info(f"{inst_name},{safe},{time_end - time_start}")
+        except TimeoutException:
+            logger_file.info(f"{inst_name},---,---")
+            break
+
+
 if __name__ == '__main__':
 
     logger_file.info('Benchmark,Over-approx.,,Mixed1,,Complete,,')
@@ -34,33 +86,30 @@ if __name__ == '__main__':
         csv_reader = csv.reader(instances)
 
         for instance in csv_reader:
-            network_instance = conv.load_network_path(f"{folder}/Networks/{instance[0]}")
-            if isinstance(network_instance, ONNXNetwork):
-                onnx_net = ONNXConverter().to_neural_network(network_instance)
+            exec_instance(f"{folder}/Networks/{instance[0]}",
+                          f"{folder}/Properties/{instance[1]}",
+                          instance[1])
 
-            property_instance = NeVerProperty()
-            property_instance.from_smt_file(f"{folder}/Properties/{instance[1]}")
+    # ACC and RL launcher
+    dirs = ['data/ACC', 'data/RL/Cartpole', 'data/RL/Lunar Lander', 'data/RL/Dubins Rejoin']
 
-            for setting in pynever_setting:
-                inst_name = str(instance).replace(',', ' -')
-                logger_stream.info(f"Benchmark: {inst_name}")
-                logger_stream.info(f"PyNeVer setting: {setting[0]}")
+    for dir_name in dirs:
+        for property_file in os.listdir(f"{dir_name}/Properties"):
+            p_f = os.path.join(f"{dir_name}/Properties", property_file)
 
-                try:
-                    with time_limit(TIMEOUT):
-                        strategy = NeverVerification(setting[1], setting[2])
-                        time_start = time.perf_counter()
-                        safe = not strategy.verify(onnx_net, property_instance)
-                        time_end = time.perf_counter()
-                        logger_file.info(f"{inst_name},{safe},{time_end - time_start}")
-                except TimeoutException as e:
-                    logger_file.info(f"{inst_name},---,---")
-                    break
+            if os.path.isfile(p_f):
+                for network_file in os.listdir(f"{dir_name}/Networks"):
+                    n_f = os.path.join(f"{dir_name}/Networks", network_file)
 
-    # acas_dir = ('data/ACAS XU')
-    # for property_file in os.listdir(f"{acas_dir}/Properties"):
-    #     f = os.path.join(acas_dir, property_file)
-    #
-    #     network_files = os.listdir(f"{acas_dir}/Networks")
-    #
-    #     if os.path.isfile(f):
+                    if os.path.isfile(n_f):
+                        exec_instance(n_f, p_f, property_file)
+
+    # Drones launcher
+    with open('data/Drones/instances.csv') as instances:
+        folder = 'data/Drones'
+        csv_reader = csv.reader(instances)
+
+        for instance in csv_reader:
+            exec_instance(f"{folder}/Networks/{instance[0]}",
+                          f"{folder}/Properties/{instance[1]}",
+                          instance[1])
