6aff26c5145982a9f965dc6789df765a06589c50
Stefano Demarchi <stefano_demarchi@outlook.com>
2023-09-19 17:03:03 +0200

    Purge Tensorflow from public release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyNeVer",
-    version="0.1.1",
+    version="0.1.1a2",
     author="Dario Guidotti",
     author_email="dario.guidotti@edu.unige.it",
     license='GNU General Public License with Commons Clause License Condition v1.0',
