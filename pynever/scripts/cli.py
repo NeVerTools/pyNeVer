0287016f43101b2a8584b288648987a64e9ea86b
pesimachete <ph.simao.achete@gmail.com>
2023-10-19 14:23:36 +0200

    Fix CLI output implementation  Closes #2

--- pynever/scripts/cli.py
+++ pynever/scripts/cli.py
@@ -4,6 +4,7 @@ import time
 
 import csv
 
+from pynever import tensor
 from pynever.networks import SequentialNetwork
 from pynever.strategies import conversion
 from pynever.strategies.conversion import ONNXNetwork, ONNXConverter
@@ -66,7 +67,7 @@ def verify_single_model(model_file: str, property_file: str, strategy: str):
 
                     ver_strategy = None
                     if strategy == 'complete':
-                        ver_strategy = NeverVerification('best_n_neurons',
+                        ver_strategy = NeverVerification('complete',
                                                          [[10000] for _ in range(network.count_relu_layers())])
                     elif strategy == 'approx':
                         ver_strategy = NeverVerification('best_n_neurons',
@@ -78,29 +79,38 @@ def verify_single_model(model_file: str, property_file: str, strategy: str):
                     model_name = os.path.basename(nn_path)
                     property_name = os.path.basename(property_file)
                     ver_start_time = time.perf_counter()
-                    list_return = ver_strategy.verify(network, to_verify)
+                    safe = ver_strategy.verify(network, to_verify)
                     ver_end_time = time.perf_counter() - ver_start_time
-                    if type(list_return) is tuple:
-                        bool_verification = list_return
-                        tensor_counterexample = list_return[1]
-                    else:
-                        tensor_counterexample = None
-                        bool_verification = list_return
-                    if not bool_verification:
+                    tensor_counterexample = None
+
+                    if safe:
                         if strategy == 'complete':
                             answer = 'Falsified'
+                            counter_stars = ver_strategy.counterexample_stars
+
+                            if counter_stars is not None:
+                                some_counterexamples = []
+                                for cex_star in counter_stars:
+                                    try:
+                                        some_counterexamples.extend(cex_star.get_samples(num_samples=1))
+                                    except:
+                                        print("Error finding the counterexample")
+                                if len(some_counterexamples) > 0:
+                                    tensor_counterexample = some_counterexamples[0]
                         else:
                             answer = 'Unknown'
                     else:
                         answer = 'Verified'
-
+                    printable_counterexample = reformat_counterexample(tensor_counterexample)
                     print("Benchmark ", model_name, ", ", property_name, "\n",
                           "Answer: ", answer, "\n",
-                          "(if Falsified) Counterexample found: ", tensor_counterexample, "\n",
                           "Time elapsed: ", ver_end_time)
+                    if answer == 'Falsified':
+                        print("Counterexample: ", printable_counterexample)
 
-                    writer = csv.writer(open('ACC/output.csv', 'a', newline=''))
-                    writer.writerow([model_name, property_name, strategy, answer, ver_end_time, tensor_counterexample])
+                    writer = csv.writer(writer_file)
+                    writer.writerow(
+                        [model_name, property_name, strategy, answer, ver_end_time, printable_counterexample])
                     return answer
             else:
                 print('The model is not an ONNX model.')
@@ -109,6 +119,8 @@ def verify_single_model(model_file: str, property_file: str, strategy: str):
 
 def verify_CSV_model(csv_file: str, strategy: str):
     csv_file_path = os.path.abspath(csv_file)
+    writer_file = open('ACC/output.csv', 'w', newline='')
+    writer_file.close()
     if not os.path.isfile(csv_file_path):
         print('Invalid path for the CSV file.')
         return False
@@ -121,6 +133,17 @@ def verify_CSV_model(csv_file: str, strategy: str):
         else:
             for row in csv_file_iti:
                 if len(row) >= 2:
-                    verify_single_model(row[0], row[1], strategy)
+                    writer_file = open('ACC/output.csv', 'a', newline='')
+                    verify_single_model(property_type, row[0], row[1], strategy, writer_file)
                 else:
                     print("This row is not valid: ", row, "\n")
+
+
+def reformat_counterexample(counterexample: tensor):
+    response = "["
+    for component in counterexample:
+        response += str(float(component))
+        response += " "
+    response = response[:-1]
+    response += "]"
+    return response
