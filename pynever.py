3282833812d43a078188ea25c5ba5cc3637ecbb4
Stefano Demarchi <stefano_demarchi@outlook.com>
2023-11-09 13:45:44 +0100

    Update scripts help

--- pynever.py
+++ pynever.py
@@ -6,29 +6,29 @@ from pynever.scripts import cli
 if __name__ == "__main__":
 
     # Parse arguments
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser('pyNeVer')
 
     parser.add_argument('strategy', choices=['approx', 'mixed', 'complete'],
-                        default='complete', help='Verification strategy to use, uses complete if not provided')
+                        default='complete', help='verification strategy to use, uses complete if not provided')
     parser.add_argument('-o', '--out', default='output.csv',
-                        help='Output file for execution log')
+                        help='output file for execution log')
 
     subparsers = parser.add_subparsers(title='Execution mode',
                                        description='verify a single property or a batch of instances')
 
-    single_parser = subparsers.add_parser('single', help='Verify a single instance')
+    single_parser = subparsers.add_parser('single', help='verify a single instance')
     group = single_parser.add_argument_group('Property type', 'expected structure of the property')
     prop_type = group.add_mutually_exclusive_group(required=True)
-    prop_type.add_argument('-s', action='store_true', help='Safety property')
-    prop_type.add_argument('-u', action='store_true', help='Unsafety property')
-    single_parser.add_argument('model', help='Network model in ONNX format')
-    single_parser.add_argument('property', help='Property specification in VNN-LIB format')
+    prop_type.add_argument('-s', '--safety', action='store_true', help='safety property')
+    prop_type.add_argument('-u', '--unsafety', action='store_true', help='unsafety property')
+    single_parser.add_argument('model', help='network model in ONNX format')
+    single_parser.add_argument('property', help='property specification in VNN-LIB format')
 
-    batch_parser = subparsers.add_parser('batch', help='Verify a batch of instances')
+    batch_parser = subparsers.add_parser('batch', help='verify a batch of instances')
     group = batch_parser.add_argument_group('Property type', 'expected structure of the property')
     prop_type = group.add_mutually_exclusive_group(required=True)
-    prop_type.add_argument('-s', action='store_true', help='Safety property')
-    prop_type.add_argument('-u', action='store_true', help='Unsafety property')
+    prop_type.add_argument('-s', '--safety', action='store_true', help='safety property')
+    prop_type.add_argument('-u', '--unsafety', action='store_true', help='unsafety property')
     batch_parser.add_argument('csv', help='CSV file that specifies the instances')
 
     args = vars(parser.parse_args())
--- pynever/tests/regtest.py
+++ pynever/tests/regtest.py
@@ -8,17 +8,17 @@ import subprocess
 # Test single instances
 
 print("Test single instance of test_small")
-subprocess.run(['python', 'pynever2.py', '-o', 'single.csv', 'approx', 'single', '-u',
+subprocess.run(['python', 'pynever.py', '-o', 'single.csv', 'approx', 'single', '-u',
                 'examples/data/regtest/test_small.onnx',
                 'examples/data/regtest/test_small.vnnlib'])
 
 print("Test single instance of test_ACC")
-subprocess.run(['python', 'pynever2.py', 'mixed', 'single', '-s',
+subprocess.run(['python', 'pynever.py', 'mixed', 'single', '-s',
                 'examples/data/regtest/test_ACC.onnx',
                 'examples/data/regtest/test_ACC.vnnlib'])
 
 # Test batch instances
 
 print("Test ACC batch instances")
-subprocess.run(['python', 'pynever2.py', 'complete', 'batch', '-s',
+subprocess.run(['python', 'pynever.py', 'complete', 'batch', '-s',
                 'examples/data/regtest/instances.csv'])
