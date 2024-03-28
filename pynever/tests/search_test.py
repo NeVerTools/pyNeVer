9018f38a10c6c2ca79c2ce2e56202b747147f253
Stefano Demarchi <stefano_demarchi@outlook.com>
2024-03-28 22:13:22 +0100

    Refactor tests and benchmarks

--- pynever/tests/search_test.py
+++ pynever/tests/search_test.py
@@ -1,12 +1,11 @@
+import csv
 import logging
-import os
 import time
 
 from pynever.networks import SequentialNetwork
 from pynever.strategies import verification, conversion
 
-NETS_PATH = '../../examples/submissions/2023_AppliedIntelligence/vnncomp_benchmarks/clean_onnx'
-PROPS_PATH = '../../examples/submissions/2023_AppliedIntelligence/vnncomp_benchmarks/vnnlib_specs'
+BENCHMARKS_PATH = '../../examples/benchmarks'
 
 logger_stream = logging.getLogger("pynever.strategies.verification")
 logger_stream.addHandler(logging.StreamHandler())
@@ -18,8 +17,9 @@ logger_file.setLevel(logging.INFO)
 
 
 def format_csv(answer: list) -> str:
+    # If answer is False without counterexample -> timeout
     if len(answer) == 1:
-        return answer[0]
+        return 'Verified' if answer[0] else 'Unknown'
 
     else:
         fancy_cex = '['
@@ -29,30 +29,18 @@ def format_csv(answer: list) -> str:
         fancy_cex = fancy_cex[:-1]
         fancy_cex += ']'
 
-        return f'{answer[0]},{fancy_cex}'
+        return f'Falsified,{fancy_cex}'
 
 
-if __name__ == '__main__':
-    networks = [f'{NETS_PATH}/{nn_name}' for nn_name in os.listdir(NETS_PATH)]
-
-    # This is amazing
-    benchmarks = {
-        # Key = name of the network
-        nn.split('/')[-1].replace('.onnx', ''):
-
-        # Value = list of properties
-            [f'{PROPS_PATH}/{p_name}' for p_name in os.listdir(PROPS_PATH)
-             if p_name.split('_')[0] in nn]
-        for nn in networks
-    }
-
+def launch_instances(instances_file: str):
     logger_file.info('Instance,Time,Result,Counterexample')
+    folder = instances_file.replace(instances_file.split('/')[-1], '')
+    with open(instances_file, 'r') as f:
+        csv_reader = csv.reader(f)
 
-    for network_name, properties in benchmarks.items():
+        for instance in csv_reader:
 
-        network_path = f'{NETS_PATH}/{network_name}.onnx'
-
-        for prop_path in properties:
+            network_path = f'{folder}/{instance[0]}'
             ver_strategy = verification.SearchVerification()
             onnx_net = conversion.load_network_path(network_path)
             if isinstance(onnx_net, conversion.ONNXNetwork):
@@ -60,13 +48,19 @@ if __name__ == '__main__':
 
                 if isinstance(net, SequentialNetwork):
                     prop = verification.NeVerProperty()
-                    prop.from_smt_file(prop_path)
+                    prop.from_smt_file(f'{folder}/{instance[1]}')
 
-                    logger_stream.info(f'Instance: {network_name} - {prop_path.split("/")[-1]}')
+                    inst_name = f'{instance[0].split("/")[-1]} - {instance[1].split("/")[-1]}'
+
+                    logger_stream.info(f'Instance: {inst_name}')
 
                     start_time = time.perf_counter()
                     result = ver_strategy.verify(net, prop)
                     lap = time.perf_counter() - start_time
 
                     logger_stream.info(f'{result} - {lap}')
-                    logger_file.info(f'{network_name} - {prop_path.split("/")[-1]},{lap},{format_csv(result)}')
+                    logger_file.info(f'{inst_name},{lap},{format_csv(result)}')
+
+
+if __name__ == '__main__':
+    launch_instances(f'{BENCHMARKS_PATH}/RL/instances.csv')
