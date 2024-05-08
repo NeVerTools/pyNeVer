ce66f36416be6980abb40dfa7161d62a58dec4a0
Karim Pedemonte <S4642024@studenti.unige.it>
2024-05-08 14:58:31 +0200

    Added submodule abstraction.

---
+++ pynever/strategies/abstraction/__init__.py
@@ -0,0 +1,8 @@
+import logging
+
+LOGGER_EMPTY = logging.getLogger("pynever.strategies.abstraction.empty_times")
+LOGGER_LP = logging.getLogger("pynever.strategies.abstraction.lp_times")
+LOGGER_LB = logging.getLogger("pynever.strategies.abstraction.lb_times")
+LOGGER_UB = logging.getLogger("pynever.strategies.abstraction.ub_times")
+
+PARALLEL = True
