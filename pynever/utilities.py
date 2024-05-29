a13d7912b78a3b0bdd0f552ee544a1fde6d03a60
KarimPedemonte <S4642024@studenti.unige.it>
2024-05-29 16:52:12 +0200

    Added datasets, training and utilities modules.

---
+++ pynever/utilities.py
@@ -0,0 +1,35 @@
+import torch
+
+import pynever.networks as networks
+from pynever.strategies.conversion.converters.pytorch import PyTorchConverter
+from pynever.tensors import Tensor
+
+
+def execute_network(network: networks.NeuralNetwork, net_input: Tensor) -> Tensor:
+    """
+    Apply the neural network function to an input Tensor
+    using pyTorch backend
+
+    Parameters
+    ----------
+    network : NeuralNetwork
+        The network to evaluate
+    net_input : Tensor
+        The input value to feed
+
+    Returns
+    ----------
+    Tensor
+        The computed output
+
+    """
+
+    input_t = torch.Tensor(net_input)
+
+    py_net = PyTorchConverter().from_neural_network(network)
+    py_net.pytorch_network.eval()
+    py_net.pytorch_network.float()
+
+    output = py_net.pytorch_network(input_t.float().T)
+
+    return output.detach().numpy().T
