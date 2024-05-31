3402327ddb9101b6b0999dde5385cf1faf7e1b94
Karim Pedemonte <S4642024@studenti.unige.it>
2024-05-31 13:13:52 +0200

    Added checks in verification parameters.

--- pynever/strategies/verification/parameters.py
+++ pynever/strategies/verification/parameters.py
@@ -8,9 +8,15 @@ class VerificationParameters(abc.ABC):
 
 
 class NeverVerificationParameters(VerificationParameters):
-    def __init__(self, heuristic: str, neurons_to_refine: list | None, approx_levels: int | list[int] | None):
+    def __init__(self, heuristic: str, neurons_to_refine: list | None = None,
+                 approx_levels: int | list[int] | None = None):
         if heuristic not in ['overapprox', 'complete', 'mixed']:
             raise Exception(f'Selected heuristic {heuristic} is not valid')
+
+        if heuristic == 'mixed' and neurons_to_refine is None:
+            raise Exception('neurons_to_refine cannot be None if heuristic is mixed')
+
+        # TODO add checks for approx_levels if needed
 
         self.heuristic = heuristic
         self.neurons_to_refine = neurons_to_refine
@@ -20,6 +26,14 @@ class NeverVerificationParameters(VerificationParameters):
 class SearchVerificationParameters(VerificationParameters):
     def __init__(self, heuristic: str = 'sequential', bounds: str = 'symbolic',
                  intersection: str = 'bounds_lp', timeout: int = 300):
+        # TODO better error messages
+        if heuristic not in ['sequential']:
+            raise Exception(f'Selected heuristic {heuristic} is not valid')
+        if bounds not in ['symbolic']:
+            raise Exception(f'Selected bounds {bounds} is not valid')
+        if intersection not in ['bounds_lp']:
+            raise Exception(f'Selected intersection {intersection} is not valid')
+
         self.heuristic = heuristic
         self.bounds = bounds
         self.intersection = intersection
