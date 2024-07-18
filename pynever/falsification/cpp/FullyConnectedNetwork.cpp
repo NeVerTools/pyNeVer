10ef2fb093b760d50b18099c479159b3707618cc
Stefano Demarchi <stefano_demarchi@outlook.com>
2024-07-18 13:37:25 +0200

    Delete falsification directory until refactoring

--- pynever/falsification/cpp/FullyConnectedNetwork.cpp
+++
@@ -1,9 +0,0 @@
-#include "FullyConnectedNetwork.h"
-
-FullyConnectedNetwork::FullyConnectedNetwork(std::vector<FullyConnectedLayer> layers) {
-    this->_layers = layers;
-}
-
-std::vector<FullyConnectedLayer> FullyConnectedNetwork::getLayers() {
-    return this->_layers;
-}
