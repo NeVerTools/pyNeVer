878d2141c0c7d36544f9ff6e524da3312cc46d68
aims-lab <stefano.demarchi@edu.unige.it>
2025-05-12 14:24:26 +0200

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyNeVer",
-    version="1.1.0",
+    version="1.1.1",
     author="Dario Guidotti, Stefano Demarchi, Andrea Gimelli, Elena Botoeva",
     author_email="dguidotti@uniss.it, stefano.demarchi@edu.unige.it, andrea.gimelli@edu.unige.it, e.botoeva@kent.ac.uk",
     license='GNU General Public License with Commons Clause License Condition v1.0',
