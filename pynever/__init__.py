3c601a0ceca1eb0d9efa59c2842664eff450487d
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-05-14 10:54:16 +0200

    Add configuration file

--- pynever/__init__.py
+++ pynever/__init__.py
@@ -1,3 +1,16 @@
+"""
+This module initializes the configuration variables for the repository
+"""
+import enum
 import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+with open('configuration.txt', 'r') as f:
+    config_data = dict()
+    for line in f:
+        l = line.strip('\n').split()
+        if '#' not in l and l != []:
+            config_data[l[0]] = l[2]
+
+Configuration = enum.Enum('Configuration', config_data)
