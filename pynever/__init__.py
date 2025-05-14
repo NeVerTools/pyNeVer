5a0e84f61a63998380ae96fb5d5d3a276c6e3941
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-05-14 12:02:53 +0200

    Refactor init

--- pynever/__init__.py
+++ pynever/__init__.py
@@ -4,13 +4,11 @@ This module initializes the configuration variables for the repository
 import enum
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
 with open(os.path.join(os.path.dirname(__file__), 'config/configuration.ini'), 'r') as f:
-    config_data = dict()
-    for line in f:
-        l = line.strip('\n').split()
-        if '#' not in l and l != []:
-            config_data[l[0]] = l[2]
+    config_data = {
+        l[0]: l[2]
+        for l in [line.strip('\n').split() for line in f]
+        if l != [] and '#' not in l
+    }
 
 Configuration = enum.Enum('Configuration', config_data)
