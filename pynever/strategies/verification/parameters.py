a7822deb14c9993399bc6243a86d6ffb62d0858e
sdemarch <stefano.demarchi@edu.unige.it>
2025-06-18 13:49:36 +0200

    Add verification parameters docs

--- pynever/strategies/verification/parameters.py
+++ pynever/strategies/verification/parameters.py
@@ -1,3 +1,9 @@
+"""
+This module contains the classes used to define verification parameters for different strategies.
+The class ``VerificationParameters`` is the abstract class from which derive the concrete classes
+``SSLPVerificationParameters`` and ``SSBPVerificationParameters``.
+"""
+
 import abc
 
 from pynever.strategies.verification.ssbp.constants import (
@@ -8,13 +14,26 @@ from pynever.strategies.verification.ssbp.constants import (
 )
 
 
-# TODO add documentation
-
 class VerificationParameters(abc.ABC):
+    """
+    This class is the abstract base for defining the verification parameters
+    related to a specific verification algorithm
+    """
     pass
 
 
 class SSLPVerificationParameters(VerificationParameters):
+    """
+    This class defines the parameters for the SSLP verification algorithm.
+
+    Attributes
+    ----------
+    heuristic: str
+        The verification heuristic (complete, mixed or approximate)
+    neurons_to_refine: int
+        The number of neurons to refine in the mixed setting
+    """
+
     def __init__(self, heuristic: str = 'complete', neurons_to_refine: list | None = None):
         if heuristic not in ['overapprox', 'complete', 'mixed']:
             raise Exception(f'Selected heuristic {heuristic} is not valid')
@@ -28,6 +47,23 @@ class SSLPVerificationParameters(VerificationParameters):
 
 
 class SSBPVerificationParameters(VerificationParameters):
+    """
+    This class defines the parameters for the SSBP verification algorithm.
+
+    Attributes
+    ----------
+    heuristic: RefinementStrategy
+        The refinement heuristic to apply
+    bounds: BoundsBackend
+        The bounds backend structure to use
+    bounds_direction: BoundsDirection
+        The direction to compute the bounds
+    intersection: IntersectionStrategy
+        The intersection strategy to use
+    timeout: int
+        The timeout in seconds
+    """
+
     def __init__(self,
                  heuristic: RefinementStrategy = RefinementStrategy.INPUT_BOUNDS_CHANGE,
                  bounds: BoundsBackend = BoundsBackend.SYMBOLIC,
