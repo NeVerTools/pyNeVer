c5d62c062da1c3887f02fc560eeff31899f24086
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-07-09 16:27:27 +0200

    Better regression test

--- test/unittests/parser_test.py
+++ test/unittests/parser_test.py
@@ -1,7 +1,13 @@
+import time
+
 from pynever.strategies.verification.properties import VnnLibProperty
 
-vnnlib = VnnLibProperty('prop_3_complex.vnnlib')
-print(vnnlib.in_matrix)
-print(vnnlib.in_bias)
-print(vnnlib.out_matrix_list)
-print(vnnlib.out_bias_list)
+for prop in ['2d_prop.vnnlib', 'prop_3_complex.vnnlib', 'prop_cifar.vnnlib']:
+    start = time.perf_counter()
+    vnnlib = VnnLibProperty(prop)
+    print(f'Parsed property {prop} in {time.perf_counter() - start:4f} seconds')
+
+    print(vnnlib.in_matrix)
+    print(vnnlib.in_bias)
+    print(vnnlib.out_matrix_list)
+    print(vnnlib.out_bias_list)
