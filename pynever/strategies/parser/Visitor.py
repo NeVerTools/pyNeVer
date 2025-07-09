6d00a5b19a26135582896b5d50ece28961eee8dd
Stefano Demarchi <stefano_demarchi@outlook.com>
2025-07-09 11:54:30 +0200

    Copy visitor of input constraints

--- pynever/strategies/parser/Visitor.py
+++ pynever/strategies/parser/Visitor.py
@@ -3,7 +3,7 @@
 import torch
 
 from pynever.strategies.parser.tree import InfoNode
-from pynever.strategies.parser.util import NodeType, Assertion
+from pynever.strategies.parser.util import NodeType, Assertion, Operation
 
 
 class Visitor:
@@ -65,7 +65,7 @@ class Visitor:
                     if op_node.type != NodeType.OPERATION:
                         raise ValueError(f'Node {node} is not an operation node, cannot evaluate it.')
 
-                    coefs, bias = self.visit_simple(op_node)
+                    coefs, bias = self.visit_input(op_node)
                     in_matrix[in_cnt, :] = torch.Tensor(coefs)
                     in_bias[in_cnt, 0] = bias
                     in_cnt += 1
@@ -77,10 +77,96 @@ class Visitor:
                 case Assertion.OUT_COMPLEX:
                     if out_visited:
                         raise ValueError(f'Output assertion {node} have already been visited')
-                    out_matrices, out_biases = self.visit_complex(node)
+                    out_matrices, out_biases = self.visit_output(node)
                     out_visited = True
 
                 case _:
                     raise ValueError(f'Unknown node type {node.type}')
 
         return in_matrix, in_bias, out_matrices, out_biases
+
+    def visit_input(self, node: InfoNode) -> tuple[list[float], float]:
+        """
+        Procedure to visit a single input constraint defined by a node
+
+        Parameters
+        ----------
+        node: InfoNode
+            Node representing a single constraint
+
+        Returns
+        -------
+        tuple[list[float], float]
+            The list of coefficients and the bias term for the constraint
+        """
+        switch_sign = False  # Flag for >= constraints
+
+        if node.type != NodeType.OPERATION:
+            raise ValueError(f'Node {node} is not an operation node, cannot evaluate it.')
+        if node.value not in [Operation.GE, Operation.LE]:
+            raise ValueError(f'Node {node} is not a comparison operation, cannot evaluate it.')
+        if node.value == Operation.GE:
+            switch_sign = True
+
+        lhs = node.children[0]
+        rhs = node.children[1]
+
+        coefs = [0] * self.n_inputs
+        bias = 0.0
+
+        # Check the left-hand side
+        match lhs.type:
+            case NodeType.IN_VAR:
+                coefs[lhs.value] += 1
+
+            case NodeType.CONST:
+                bias -= lhs.value
+
+            case NodeType.OPERATION:
+                c, b, is_variable = self.visit_op(lhs)
+                if is_variable:
+                    for i in range(self.n_inputs):
+                        coefs[i] += c[i]  # TODO check se può sostituire
+                    bias -= b
+
+            case _:
+                raise ValueError(f'Left-hand side {lhs} is not a valid operand')
+
+        # Check the right-hand side
+        match rhs.type:
+            case NodeType.IN_VAR:
+                coefs[rhs.value] -= 1
+
+            case NodeType.CONST:
+                bias += rhs.value
+
+            case NodeType.OPERATION:
+                c, b, is_variable = self.visit_op(rhs)
+                if is_variable:
+                    for i in range(self.n_inputs):
+                        coefs[i] -= c[i]
+                    bias += b
+
+            case _:
+                raise ValueError(f'Right-hand side {rhs} is not a valid operand')
+
+        if switch_sign:
+            coefs = [-c for c in coefs]
+            bias = -bias
+
+        return coefs, bias
+
+    def visit_output(self, node: InfoNode) -> tuple[list[list[float]], list[float]]:
+        """
+
+        Parameters
+        ----------
+        node: InfoNode
+            Node representing multiple constraints
+
+        Returns
+        -------
+        tuple[list[list[float]], list[float]]
+            The list of output matrices and biases for the constraints
+        """
+        pass
